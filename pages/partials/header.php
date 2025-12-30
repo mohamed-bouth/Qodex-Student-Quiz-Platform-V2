7fde0c11b70c86e4d4f2b09c9d360507d7d98ab2
mohamed_bouth <mohamedbouth87@gmail.com>
2025-12-30 14:48:04 +0100

    QOD-5 create the nav bar of student space

--- pages/partials/header.php
+++ pages/partials/header.php
@@ -13,4 +13,4 @@
     <script src="https://cdn.tailwindcss.com"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
 </head>
-<body class="bg-gray-50">
+<body class="bg-gray-50 ">
